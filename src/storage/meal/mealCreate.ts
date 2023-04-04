3a2d6b188d4bc8cf69d4e55c3c936e6ceffac5b2
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-04-03 22:56:09 -0300

    feat: Implementado o AsyncStorage e criado as ações de criação de refeições e listagem

---
+++ src/storage/meal/mealCreate.ts
@@ -0,0 +1,22 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { MEAL_COLLECTION } from "@storage/storedConfig";
+import { mealsGetAll } from "./mealsGetAll";
+
+export type MealProps = {
+  name: string;
+  description: string;
+  date: string;
+  time: string;
+  inDiet: boolean;
+}
+
+export async function mealCreate(newMeal: MealProps) {
+  try {
+    const storedMeals = await mealsGetAll();
+
+    const storage = JSON.stringify([...storedMeals, newMeal])
+    await AsyncStorage.setItem(MEAL_COLLECTION, storage);
+  } catch (error) {
+    throw new Error('Algo deu errado!');
+  }
+}
