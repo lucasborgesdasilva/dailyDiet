e8a4e708fad378440ffc78d0e90b969ff566167d
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-04-03 22:47:17 -0300

    feat: Adicionando o Design System do projeto

---
+++ src/theme/index.tsx
@@ -0,0 +1,43 @@
+export default {
+  colors: {
+    red: {
+      dark: '#BF3B44',
+      regular: '#F3BABD',
+      light: '#F4E6E7',
+    },
+
+    green: {
+      dark: '#639339',
+      regular: '#CBE4B4',
+      light: '#E5F0DB',
+    },
+
+    gray: {
+      '100': '#1B1D1E',
+      '200': '#333638',
+      '300': '#5C6265',
+      '400': '#B9BBBC',
+      '500': '#DDDEDF',
+      '600': '#EFF0F0',
+      '700': '#FAFAFA',
+    },
+
+    white: '#FFFFFF',
+  },
+
+  fonts: {
+    family: {
+      regular: 'NunitoSans_400Regular',
+      bold: 'NunitoSans_700Bold',
+    },
+
+    size: {
+      xs: '12',
+      sm: '14',
+      md: '16',
+      lg: '18',
+      xl: '24',
+      xxl: '32',
+    },
+  }
+}
