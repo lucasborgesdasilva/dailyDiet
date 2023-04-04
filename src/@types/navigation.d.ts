de54d86155190d8e248c15ee29c4c36d92a2a314
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-04-03 22:50:03 -0300

    feat: Adicionando as tipagens do StyledComponents, imagens com extensão .png e as screens disponíveis na navegação das páginas
    Esse commit adiciona as tipagens para facilitar a utilização dos recursos.

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,13 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      meals: undefined;
+      newMeal: undefined;
+      details: undefined;
+      message: {
+        inDiet: boolean;
+      }
+    }
+  }
+}
