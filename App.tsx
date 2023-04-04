553ee6a9b2edf712551b9b4c757427e754f88907
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-04-03 22:46:08 -0300

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text } from 'react-native';
+
+import { ThemeProvider } from 'styled-components/native';
+
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
+
+import { Routes } from '@routes/index';
+
+import theme from './src/theme';
+
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        backgroundColor="transparent" 
+        barStyle="dark-content" 
+        translucent 
+      />
+      { fontsLoaded ? <Routes /> : <Text>Carregando</Text> }
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
